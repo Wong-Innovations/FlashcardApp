6e2d501a42863fb8d0ec0bd9269b6c356b09cc2e
Wong-Innovations <dylanwong@nevada.unr.edu>
2021-05-10 22:56:01 -0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,96 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Modal, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+
+import Flashcard from './components/Flashcard';
 
 export default function App() {
+
+  const [flashcard, setFlashcards] = useState([]);
+  const [guess, setGuess] = useState('');
+  const [cardCompleted, setCardCompleted] = useState(false);
+
+  const answer = "genki";
+
+  const handleCheckGuess = () => {
+    Keyboard.dismiss();
+    if (guess == answer) {
+      setCardCompleted(true);
+      setTimeout(() => {
+        handleChangeCard();
+        setCardCompleted(false);
+      } , 1500);
+    }
+    setGuess('');
+  }
+
+  const handleChangeCard = () => {
+
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {/* Flashcard Wrapper */}
+      <View style={styles.flashcardWrapper}>
+        <Text style={styles.sectionTitle}>My Flashcards</Text>
+
+        <View style={styles.flashcard}>
+          <Flashcard
+            main={['元気', '元気ですか。', '元気です。']}
+            secondary={['', 'Are you well?', 'I am well.']}
+          />
+          {/* <Flashcard main={'genki'} secondary={'genki'} /> */}
+        </View>
+
+      </View>
+
+      {/* Answer Verification */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.answerWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'Answer'}
+          value={guess}
+          onChangeText={text => setGuess(text)}
+        />
+
+        <TouchableOpacity onPress={handleCheckGuess}>
+          <View style={styles.buttonWrapper}>
+            <Text style={styles.buttonText}>Check</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+      {/* Success Modal */}
+      <Modal
+        animationType={'slide'}
+        transparent={true}
+        visible={cardCompleted}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modal}>
+            <Icon
+              reverse
+              name={'check'}
+              type={'font-awesome'}
+              color={"#32cd32"}
+              reverseColor={'#FFF'}
+              size={30}
+            />
+            <Text style={styles.modalText}>Good Job!</Text>
+          </View>
+        </View>
+        
+      </Modal>
+      
     </View>
   );
 }
@@ -14,8 +98,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
+  },
+  flashcardWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  flashcard: {
+    marginTop: 25,
+  },
+  answerWrapper: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+  },
+  buttonWrapper: {
+    width: 100,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modal: {
+    width: 250,
+    height: 200,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 40,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontSize: 16,
   },
 });
---
+++ components/Flashcard.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Flashcard = (props) => {
+
+  const [cardNumber, setCardNumber] = useState(0);
+
+  const handleCycleFlashcard = () => {
+    if (cardNumber < props.main.length-1)
+      setCardNumber(cardNumber+1);
+    else
+      setCardNumber(0);
+  }
+
+  return (
+    <TouchableOpacity onPress={handleCycleFlashcard}>
+      <View style={styles.flashcard}>
+        <Text style={styles.main}>{props.main[cardNumber]}</Text>
+        <Text style={styles.secondary}>{props.secondary[cardNumber]}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  flashcard: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+    paddingVertical: 100,
+  },
+  main: {
+    fontSize: 36,
+    fontWeight: 'bold',
+  },
+  secondary: {
+    marginTop: 10,
+  },
+});
+
+export default Flashcard;
